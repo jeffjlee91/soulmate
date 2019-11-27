593686c041bf786afbacfad8fcc69373ae831e50
Felix Wenhao Wang <felixwhwang@gmail.com>
2019-11-27 13:00:44 -0800

    Update app.jsx

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import DetailedProfileView from './detailed-profile-view';
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <DetailedProfileView />
+      <app />
     );
   }
 }
