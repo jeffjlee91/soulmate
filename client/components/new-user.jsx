5f15d8e6296ef3968b3fa5ecae7e288286e935ca
wenhao wang <felixwhwang@gmail.com>
2019-11-29 02:23:56 +0000

    frontend user input info ready

--- client/components/new-user.jsx
+++ client/components/new-user.jsx
@@ -4,9 +4,40 @@ export default class NewUser extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      imageFile: {},
-      filePath: ''
+      newUser: {
+        email: '',
+        password: '',
+        images: '',
+        firstName: '',
+        lastName: '',
+        gender: '',
+        city: '',
+        state: '',
+        age: '',
+        height: '',
+        jobTitle: '',
+        ethnicity: '',
+        religion: '',
+        iLike: '',
+        iAm: '',
+        iAppreciate: ''
+      },
+      imageFile: {}
     };
+    this.inputHandler = this.inputHandler.bind(this);
+  }
+
+  createHandler() {
+    const req = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(this.state.newUser)
+    };
+    fetch('api/new-user', req)
+      .then(res => res.json())
+      .then(result => {
+        // ....
+      }).catch(err => alert('createHandler error', err));
   }
 
   fileSelectHandler(event) {
@@ -17,7 +48,11 @@ export default class NewUser extends React.Component {
 
   uploadHandler(event) {
     const fd = new FormData();
-    fd.append('image', this.state.imageFile, this.state.imageFile.name.split('.').splice(1).join(''));
+    fd.append(
+      'image',
+      this.state.imageFile,
+      this.state.imageFile.name.split('.').splice(1).join('')
+    );
     const req = {
       method: 'POST',
       body: fd
@@ -25,12 +60,18 @@ export default class NewUser extends React.Component {
     fetch('/api/upload-image', req)
       .then(res => res.json())
       .then(result => {
-        this.setState({
-          filePath: result.split('/').slice(2).join('/')
-        });
+        const newUser = { ...this.state.newUser };
+        newUser.images = result.split('/').slice(2).join('/');
+        this.setState({ newUser });
       })
       .catch(err => alert('uploaderHandler error', err));
     event.preventDefault();
+  }
+
+  inputHandler(event) {
+    const newUser = { ...this.state.newUser };
+    newUser[event.target.name] = event.target.value;
+    this.setState({ newUser });
   }
 
   render() {
@@ -42,7 +83,7 @@ export default class NewUser extends React.Component {
 
         <form onSubmit={this.uploadHandler.bind(this)}>
           <div className="form-group row">
-            <img src={this.state.filePath} className="photo-size col-12" />
+            <img src={this.state.newUser.images} className="photo-size col-12" />
           </div>
           <div className="form-group row d-flex justify-content-center">
             <input type="file" className="btn btn-secondary col-8" onChange={this.fileSelectHandler.bind(this)}/>
@@ -50,42 +91,68 @@ export default class NewUser extends React.Component {
           </div>
         </form>
 
-        <form>
+        <form onSubmit={this.createHandler.bind(this)}>
           <div className="form-group">
             <label htmlFor="email">Email</label>
             <input type="email" className="form-control form-rounded input" id="email"
-              placeholder="email" minLength='1' required/>
+              name="email"
+              value={this.state.newUser.email}
+              onChange={this.inputHandler}
+              placeholder="email"
+              minLength='1'
+              required/>
           </div>
 
           <div className="form-group">
             <label htmlFor="password">Password</label>
             <input type="password" className="form-control form-rounded input" id="password"
-              placeholder="password" minLength='8' required />
-            <div className="invalid-feedback">At least 8 characters!</div>
+              name="password"
+              value={this.state.newUser.password}
+              onChange={this.inputHandler}
+              placeholder="password"
+              minLength='4'
+              required />
+            <div className="invalid-feedback">At least 4 characters!</div>
           </div>
 
           <div className="form-group row">
             <div className="col-6">
               <label htmlFor="firstName">First Name</label>
               <input type="text" className="form-control form-rounded input" id="firstName"
-                placeholder="First name" minLength='1' required />
+                name="firstName"
+                value={this.state.newUser.firstName}
+                onChange={this.inputHandler}
+                placeholder="First name"
+                minLength='1'
+                required />
               <div className="invalid-feedback">Cannot be empty!</div>
             </div>
             <div className="col-6">
               <label htmlFor="lastName">Last Name</label>
               <input type="text" className="form-control form-rounded input" id="lastName"
-                placeholder="Last name" minLength='1' required />
+                name="lastName"
+                value={this.state.newUser.lastName}
+                onChange={this.inputHandler}
+                placeholder="Last name"
+                minLength='1'
+                required />
               <div className="invalid-feedback">Cannot be empty!</div>
             </div>
           </div>
 
           <div className="form-group">
             <div className="form-check form-check-inline">
-              <input className="form-check-input" type="radio" name="gender" id="male" value="male" />
+              <input className="form-check-input" type="radio"
+                name="gender"
+                value="Male"
+                onChange={this.inputHandler} />
               <label className="form-check-label" htmlFor="male">Male</label>
             </div>
             <div className="form-check form-check-inline">
-              <input className="form-check-input" type="radio" name="gender" id="female" value="female" />
+              <input className="form-check-input" type="radio"
+                name="gender"
+                value="Female"
+                onChange={this.inputHandler} />
               <label className="form-check-label" htmlFor="female">Female</label>
             </div>
           </div>
@@ -94,13 +161,24 @@ export default class NewUser extends React.Component {
             <div className="col-8">
               <label htmlFor="city">City</label>
               <input type="text" className="form-control form-rounded input" id="city"
-                placeholder="City" minLength='1' required />
+                name="city"
+                value={this.state.newUser.city}
+                onChange={this.inputHandler}
+                placeholder="City"
+                minLength='1'
+                required />
               <div className="invalid-feedback">Cannot be empty!</div>
             </div>
             <div className="col-4">
-              <label htmlFor="State">State</label>
-              <input type="text" className="form-control form-rounded input" id="State"
-                placeholder="CA" minLength='2' maxLength='2' required />
+              <label htmlFor="state">State</label>
+              <input type="text" className="form-control form-rounded input" id="state"
+                name="state"
+                value={this.state.newUser.state}
+                onChange={this.inputHandler}
+                placeholder="CA"
+                minLength='2'
+                maxLength='2'
+                required />
               <div className="invalid-feedback">Cannot be empty!</div>
             </div>
           </div>
@@ -109,13 +187,24 @@ export default class NewUser extends React.Component {
             <div className="col-6">
               <label htmlFor="age">Age</label>
               <input type="number" className="form-control form-rounded input" id="age"
-                placeholder="Age" minLength='1' maxLength='2' required />
+                name="age"
+                value={this.state.newUser.age}
+                onChange={this.inputHandler}
+                placeholder="Age"
+                minLength='1'
+                maxLength='3'
+                required />
               <div className="invalid-feedback">Cannot be empty!</div>
             </div>
             <div className="col-6">
               <label htmlFor="height">Height</label>
               <input type="text" className="form-control form-rounded input" id="height"
-                placeholder="5&apos;11&quot;" minLength='1' required />
+                name="height"
+                value={this.state.newUser.height}
+                onChange={this.inputHandler}
+                placeholder="5&apos;11&quot;"
+                minLength='1'
+                required />
               <div className="invalid-feedback">Cannot be empty!</div>
             </div>
           </div>
@@ -123,14 +212,22 @@ export default class NewUser extends React.Component {
           <div className="form-group">
             <label htmlFor="jobTitle">Job Title</label>
             <input type="text" className="form-control form-rounded input" id="jobTitle"
-              placeholder="Job Title" minLength='1' required />
+              name="jobTitle"
+              value={this.state.newUser.jobTitle}
+              onChange={this.inputHandler}
+              placeholder="Job Title"
+              minLength='1'
+              required />
             <div className="invalid-feedback">Cannot be empty!</div>
           </div>
 
           <div className="form-group row">
             <div className="col-6">
               <label htmlFor="ethnicity">Ethnicity</label>
-              <select id="ethinicity" className="custom-select input">
+              <select id="ethinicity" className="custom-select input"
+                name="ethnicity"
+                value={this.state.newUser.ethnicity}
+                onChange={this.inputHandler}>
                 <option defaultValue>Choose...</option>
                 <option value="Asian">Asian</option>
                 <option value="Caucasian">Caucasian</option>
@@ -141,7 +238,10 @@ export default class NewUser extends React.Component {
             </div>
             <div className="col-6">
               <label htmlFor="religion">Religion</label>
-              <select id="religion" className="custom-select input">
+              <select id="religion" className="custom-select input"
+                name="religion"
+                value={this.state.newUser.religion}
+                onChange={this.inputHandler}>
                 <option defaultValue>Choose...</option>
                 <option value="Christian">Christian</option>
                 <option value="Buddhist">Buddhist</option>
@@ -156,23 +256,39 @@ export default class NewUser extends React.Component {
           <div className="form-group">
             <label htmlFor="iAm">I am...</label>
             <textarea rows="2" className="form-control form-rounded input" id="iAm"
-              placeholder="One sentence about yourself" minLength='1' required />
+              name="iAm"
+              value={this.state.newUser.iAm}
+              onChange={this.inputHandler}
+              placeholder="One sentence about yourself"
+              minLength='1'
+              required />
             <div className="invalid-feedback">Cannot be empty!</div>
           </div>
 
           <div className="form-group">
             <label htmlFor="iLike">I like...</label>
             <textarea rows="4" className="form-control form-rounded input" id="iLike"
-              placeholder="More description for yourself, let other users know more about you" minLength='1' required />
+              name="iLike"
+              value={this.state.newUser.iLike}
+              onChange={this.inputHandler}
+              placeholder="More description for yourself, let other users know more about you"
+              minLength='1'
+              required />
             <div className="invalid-feedback">Cannot be empty!</div>
           </div>
 
           <div className="form-group">
             <label htmlFor="iAppreciate">I appreciate my soulmate...</label>
             <textarea rows="4" className="form-control form-rounded input" id="iAppreciate"
-              placeholder="More description about your ideal other half, like what your soulmate should looks like" minLength='1' required />
+              name="iAppreciate"
+              value={this.state.newUser.iAppreciate}
+              onChange={this.inputHandler}
+              placeholder="More description about your ideal other half, like what your soulmate should looks like"
+              minLength='1'
+              required />
             <div className="invalid-feedback">Cannot be empty!</div>
           </div>
+
           <div className="d-flex justify-content-center">
             <button type="submit" className="btn btn-primary button">Create</button>
             <button type="button" className="btn btn-secondary ml-5 button">Back</button>
