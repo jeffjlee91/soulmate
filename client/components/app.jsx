80ceb7f1d06754be6da76d1e2b639548f5b6832d
michael5522 <michaelchang.kuo@gmail.com>
2019-11-29 19:27:08 +0000

    stash

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import MainLogin from './main-login';
 
+export default class App extends React.Component {
   render() {
     return (
-      <h1>hello</h1>
+      <MainLogin />
     );
   }
 }
