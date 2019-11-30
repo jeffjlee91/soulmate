004a9b7ba010ea1d539fe92dd0df3bc6900d4d41
Jeff Lee <jeffjlee91@yahoo.com>
2019-11-29 16:12:50 -0800

    added a back button at top left

--- client/components/filter.jsx
+++ client/components/filter.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 class Filter extends React.Component {
   render() {
-    return null;
+    return (
+      <div className='container bg-color'>
+        <nav className="bg-color fixed-top navbar d-flex justify-content-between align-items-center">
+          <i className="fas fa-angle-left fas-size p-2"></i>
+        </nav>
+      </div>
+    );
   }
 }
 
