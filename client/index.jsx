5ec6b0006271dc1ad5d82506b1f2b6c739b0c58a
Jeff Lee <jeffjlee91@yahoo.com>
2019-11-29 16:10:38 -0800

    made basic html skeleton for filter

--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './components/app';
-import DiscoverPage from './components/discoverPage';
+// import DiscoverPage from './components/discoverPage';
+// import Menu from './components/menu';
+import Filter from './components/filter';
 
 ReactDOM.render(
-  <DiscoverPage />,
+  <Filter />,
   document.querySelector('#root')
 );
