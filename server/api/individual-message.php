ba2f8e8557b82e7281bde604e9cf9899867c7ea6
wenhao wang <felixwhwang@gmail.com>
2019-12-02 06:21:55 +0000

    individual-message-backend get finished

--- server/api/individual-message.php
+++ server/api/individual-message.php
@@ -15,12 +15,35 @@ if($request['method'] === 'POST') {
     $stmt->bind_param("dds", $idFrom, $idTo, $message);
     $stmt->execute();
 
-    $sqlGetInsertMessage =
-    "SELECT * FROM messages
-     WHERE messages.idFrom = $idFrom AND messages.idTo = $idTo";
-    $newMessageObj = $link->query($sqlGetInsertMessage);
-    $newMessage = mysqli_fetch_assoc($newMessageObj);
-    $response['body'] = $newMessage;
+    // $sqlGetInsertMessage =
+    // "SELECT * FROM messages
+    //  WHERE messages.createdAt = $idFrom AND messages.idTo = $idTo";
+    // $newMessageObj = $link->query($sqlGetInsertMessage);
+    // $newMessage = mysqli_fetch_assoc($newMessageObj);
+    $response['body'] = 'insert successfully';
   }
+  send($response);
+}
+
+if($request['method'] === 'GET') {
+  $idFrom = intval($request['query']['idFrom']);
+  $idTo = intval($request['query']['idTo']);
+
+  if(isset($idFrom) && is_numeric($idFrom) && $idFrom !==0
+  && isset($idTo) && is_numeric($idTo) && $idTo !==0) {
+    $sqlGetMessage =
+    "SELECT m.idFrom, m.message, u.images, m.createdAt
+     FROM messages AS m
+     JOIN users AS u
+     ON u.userId = m.idFrom
+     WHERE (m.idFrom = $idFrom AND m.idTo = $idTo)
+     OR (m.idFrom = $idTo AND m.idTo = $idFrom)";
+    $messageObj = $link->query($sqlGetMessage);
+    $messages = mysqli_fetch_all($messageObj, MYSQLI_ASSOC);
+    $response['body'] = $messages;
+  } else {
+    throw new ApiError('Something wrong with request query', 400);
+  }
+
   send($response);
 }
