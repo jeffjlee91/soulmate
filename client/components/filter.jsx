246f5c73b606123b8a9953b7eb0eb814d4d237c1
Jeff Lee <jeffjlee91@yahoo.com>
2019-12-02 16:26:54 -0800

    filter

--- client/components/filter.jsx
+++ client/components/filter.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import AgeSlider from './ageSlider';
 
 class Filter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      minAge: 20,
-      maxAge: 30
+      ageValue: '',
+      city: '',
+      state: '',
+      location: '',
+      ethinicity: '',
+      religion: '',
+      ageMin: 18,
+      ageMax: 40,
+      heightMinFeet: 4,
+      heightMinInch: 0,
+      heightMaxFeet: 7,
+      heightMaxInch: 11
     };
   }
 
@@ -14,10 +25,34 @@ class Filter extends React.Component {
       <div className='container bg-color'>
         <nav className="bg-color fixed-top navbar d-flex justify-content-between align-items-center">
           <i className="fas fa-angle-left fas-size p-2"></i>
-          <h3 className=''>Filter</h3>
+          <h3 className='paddingRight'>Filter</h3>
         </nav>
-        <div className="form-group row filterlist">
-          <div className="col-12">
+
+        <div className="form-group filterlist">
+          <div className="form-group row filterLeftPadding">
+            <div className="col-7">
+              <label htmlFor="city">City</label>
+              <input type="text" className="form-control form-rounded input" id="city"
+                name="city"
+                value={this.state.city}
+                placeholder="City"
+                minLength='1'
+                required />
+              <div className="invalid-feedback">Cannot be empty!</div>
+            </div>
+            <div className="col-3">
+              <label htmlFor="state">State</label>
+              <input type="text" className="form-control form-rounded input" id="state"
+                name="state"
+                value={this.state.state}
+                placeholder="CA"
+                minLength='2'
+                maxLength='2'
+                required />
+              <div className="invalid-feedback">Cannot be empty!</div>
+            </div>
+          </div>
+          <div className="col-12 form-group">
             <label htmlFor="religion">Location</label>
             <select id="religion" className="custom-select input"
               name="religion"
@@ -33,7 +68,7 @@ class Filter extends React.Component {
               <option value="None">None</option>
             </select>
           </div>
-          <div className="col-12 filterOptionMargin">
+          <div className="col-12 filterOptionMargin form-group">
             <label htmlFor="ethnicity">Ethnicity</label>
             <select id="ethinicity" className="custom-select input"
               name="ethnicity"
@@ -48,7 +83,7 @@ class Filter extends React.Component {
               <option value="Middle Eastern">Middle Eastern</option>
             </select>
           </div>
-          <div className="col-12 filterOptionMargin">
+          <div className="col-12 filterOptionMargin form-group">
             <label htmlFor="religion">Religion</label>
             <select id="religion" className="custom-select input"
               name="religion"
@@ -65,8 +100,91 @@ class Filter extends React.Component {
             </select>
           </div>
           <div className="col-12 slidecontainer filterOptionMargin">
-            <input type="range" min="18" max="50" value="20" className="slider" id="myRange"/>
-            <input type="range" min="18" max="50" value="30" className="slider" id="myRange"/>
+            <label htmlFor="age">Age</label>
+            <AgeSlider className='slider'/>
+          </div>
+
+          <h7 className='filterLeftPadding filterBotMargin'>Height</h7>
+
+          <div className="form-group row filterLeftPadding">
+            <div className="col-4 filterOptionMargin form-group topMargin">
+              <label htmlFor="Height"></label>
+              <select className="custom-select input"
+                name="Height"
+                // value={this.state.minHeightFeet}
+                // onChange={this.inputHandler}>
+              >
+                <option defaultValue>Feet</option>
+                <option value='4'>4 ft</option>
+                <option value="5">5 ft</option>
+                <option value="6">6 ft</option>
+                <option value="7">7 ft</option>
+              </select>
+            </div>
+            <div className="col-4 filterOptionMargin topMargin form-group">
+              <label htmlFor="Height"></label>
+              <select className="custom-select input"
+                name="Height"
+                // value={this.state.newUser.ethnicity}
+                // onChange={this.inputHandler}>
+              >
+                <option defaultValue>Inch</option>
+                <option value="0">0 in</option>
+                <option value="1">1 in</option>
+                <option value="2">2 in</option>
+                <option value="3">3 in</option>
+                <option value="4">4 in</option>
+                <option value="5">5 in</option>
+                <option value="6">6 in</option>
+                <option value="7">7 in</option>
+                <option value="8">8 in</option>
+                <option value="9">9 in</option>
+                <option value="10">10 in</option>
+                <option value="11">11 in</option>
+              </select>
+            </div>
+
+            <div className='heightRangeTo'>To</div>
+
+            <div className="col-4 filterOptionMargin form-group">
+              <label htmlFor="Height"></label>
+              <select className="custom-select input"
+                name="Height"
+                // value={this.state.newUser.ethnicity}
+                // onChange={this.inputHandler}>
+              >
+                <option defaultValue>Feet</option>
+                <option value='4'>4 ft</option>
+                <option value="5">5 ft</option>
+                <option value="6">6 ft</option>
+                <option value="7">7 ft</option>
+              </select>
+            </div>
+            <div className="col-4 filterOptionMargin form-group">
+              <label htmlFor="Height"></label>
+              <select className="custom-select input"
+                name="Height"
+                // value={this.state.newUser.ethnicity}
+                // onChange={this.inputHandler}>
+              >
+                <option defaultValue>Inch</option>
+                <option value="0">0 in</option>
+                <option value="1">1 in</option>
+                <option value="2">2 in</option>
+                <option value="3">3 in</option>
+                <option value="4">4 in</option>
+                <option value="5">5 in</option>
+                <option value="6">6 in</option>
+                <option value="7">7 in</option>
+                <option value="8">8 in</option>
+                <option value="9">9 in</option>
+                <option value="10">10 in</option>
+                <option value="11">11 in</option>
+              </select>
+            </div>
+          </div>
+          <div className="col-12">
+            <button type="submit" className="btn btn-outline-secondary saveButton">Save</button>
           </div>
         </div>
       </div>
--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './components/app';
-// import DiscoverPage from './components/discoverPage';
+// import DiscoverPage from './components/discover-page';
 // import Menu from './components/menu';
 import Filter from './components/filter';
+// import AgeSlider from './components/ageSlider';
 
 ReactDOM.render(
   <Filter />,
