e0977dc67d324f9e96a5df6d92f66ff36f4109cf
Jeff Lee <jeffjlee91@yahoo.com>
2019-12-02 16:57:07 -0800

    name changed for age-slider

--- client/index.jsx
+++ client/index.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './components/app';
-// import DiscoverPage from './components/discover-page';
-// import Menu from './components/menu';
 import Filter from './components/filter';
-// import AgeSlider from './components/ageSlider';
 
 ReactDOM.render(
   <Filter />,
