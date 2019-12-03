f12bc6e2dc45f5026840f99557d82faead3ef81e
wenhao wang <felixwhwang@gmail.com>
2019-12-03 00:55:07 +0000

    individual message react finished

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -10,7 +10,7 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       view: {
-        name: 'main',
+        name: 'individual-message',
         params: {}
       }
     };
